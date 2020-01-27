87a4c567ce9259bb5968ca74958cac5787398c8e
Wil Pirino <wpirino1@gmail.com>
2020-01-27 16:32:03 -0700

    PROJECT IS SETUP

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { strategy: 'excludeAll' } }));
   await app.listen(process.env.PORT || 3000);
