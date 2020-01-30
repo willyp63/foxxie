22090e5d58f6b4d771dc7f6a61d281f8a495ec17
Wil Pirino <wpirino1@gmail.com>
2020-01-29 22:22:25 -0700

    added footer and tweeked nav-bar logo

--- ui/src/app/shared/components/index.ts
+++ ui/src/app/shared/components/index.ts
@@ -1,10 +1,13 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TextfieldComponent } from './textfield/textfield.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const components: any[] = [
     NavBarComponent,
     TextfieldComponent,
+    FooterComponent,
 ];
 
 export * from './nav-bar/nav-bar.component';
 export * from './textfield/textfield.component';
+export * from './footer/footer.component';
